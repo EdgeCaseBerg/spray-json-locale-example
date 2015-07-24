40638c34fd512779ca509451409011832c1a8c72
Ethan Joachim Eldridge <ejayeldridge@gmail.com>
2015-07-24 00:44:03 -0400

    Update example2

--- example2.scala
+++ example2.scala
@@ -9,6 +9,31 @@ case class Thing1(msg: String, thing1Thing: String) extends Base(msg)
 
 case class Thing2(msg: String, thing2Thing: String) extends Base(msg)
 
+case class ServiceResponse(status: Int, result: Base)
+
+object StandAloneBaseConversion extends DefaultJsonProtocol {
+
+	  implicit object ColorJsonFormat extends RootJsonFormat[Base] {
+	    def write(c: Base) = c match {
+	    	case s: Thing2 => JsObject(("msg",JsString(s.msg)), ("thing2Thing",JsString(s.thing2Thing)))
+	    	case s: Thing1 => JsObject(("msg",JsString(s.msg)), ("thing1Thing",JsString(s.thing1Thing)))
+	    	case _ => serializationError(s"Could not write object $c")
+	    }
+
+	   	def read(json: JsValue) = {
+				json match {
+					case JsObject(map) => 
+						List("msg","thing1Thing","thing2Thing").map(i => map.contains(i)).toArray match {
+							case Array(true,true,false) => Thing1(map("msg").toString, map("thing1Thing").toString)
+							case Array(true,false,true) => Thing2(map("msg").toString, map("thing2Thing").toString)
+							case _ => deserializationError("fields invalid")
+						}
+					case _ => deserializationError("Base expected")
+				}
+			}
+	  }
+	}
+
 /** run me from the shell via: val s = new example.ExampleInheritanceSerialization(); s.show */
 class ExampleInheritanceSerialization {
 
@@ -37,8 +62,13 @@ class ExampleInheritanceSerialization {
 	}
 	def show() {
 		implicit val conversion = new BaseConversion()
+		implicit val srConv = jsonFormat2(ServiceResponse)
 		val res = """{"msg":"Which will this deserialize to?","thing2Thing":"Yup, thing2!"}""".parseJson.convertTo[Thing2]
 		println(res)
+
+		 val x = """{"status" : 200, "result" :{"msg":"a","thing1Thing":"t"}}""".parseJson.convertTo[ServiceResponse]
+		 println(x)
+
 	}
 
 }
